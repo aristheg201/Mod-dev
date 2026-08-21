397842df8033723ff3c563d52b617d48fff7e8b9
aristheg201 <arisgrindel@gmail.com>
2026-08-21 15:50:55 +0700

    build: disable remapping for Cobblemon mixin targets
    Mark Cobblemon-only mixin targets and accessors as non-remapped so Mixin AP does not try to resolve external mod methods through Yarn mappings.

--- lively-npcs-integration/src/main/java/vn/svframe/lively/integration/mixin/AIBattleActorAccessor.java
+++ lively-npcs-integration/src/main/java/vn/svframe/lively/integration/mixin/AIBattleActorAccessor.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(AIBattleActor.class)
+@Mixin(value = AIBattleActor.class, remap = false)
 public interface AIBattleActorAccessor {
     @Mutable
-    @Accessor("battleAI")
+    @Accessor(value = "battleAI", remap = false)
     void lively$setBattleAI(BattleAI battleAI);
 }
--- lively-npcs-integration/src/main/java/vn/svframe/lively/integration/mixin/NPCBattleActorMixin.java
+++ lively-npcs-integration/src/main/java/vn/svframe/lively/integration/mixin/NPCBattleActorMixin.java
@@ -16,21 +16,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Mixin(NPCBattleActor.class)
+@Mixin(value = NPCBattleActor.class, remap = false)
 public abstract class NPCBattleActorMixin {
-    @Inject(method = "<init>(Lcom/cobblemon/mod/common/entity/npc/NPCEntity;Ljava/util/List;ILcom/cobblemon/mod/common/api/battles/model/ai/BattleAI;)V", at = @At("TAIL"))
+    @Inject(method = "<init>(Lcom/cobblemon/mod/common/entity/npc/NPCEntity;Ljava/util/List;ILcom/cobblemon/mod/common/api/battles/model/ai/BattleAI;)V", at = @At("TAIL"), remap = false)
     private void lively$installCombatAI(NPCEntity npc, List<? extends BattlePokemon> pokemonList, int skill,
                                         BattleAI original, CallbackInfo ci) {
         if (!npc.getCommandTags().contains("lively") && !npc.getCommandTags().contains("lively_combat")) return;
         ((AIBattleActorAccessor) (Object) this).lively$setBattleAI(new LivelyCobblemonBattleAI(npc.getUuid(), skill));
     }
 
-    @Inject(method = "win", at = @At("TAIL"))
+    @Inject(method = "win", at = @At("TAIL"), remap = false)
     private void lively$rememberWin(List<? extends BattleActor> winners, List<? extends BattleActor> losers, CallbackInfo ci) {
         lively$rememberOutcome("battle_won", losers);
     }
 
-    @Inject(method = "lose", at = @At("TAIL"))
+    @Inject(method = "lose", at = @At("TAIL"), remap = false)
     private void lively$rememberLoss(List<? extends BattleActor> winners, List<? extends BattleActor> losers, CallbackInfo ci) {
         lively$rememberOutcome("battle_lost", winners);
     }
