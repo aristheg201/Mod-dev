7388f68d071691a0441ab53bfa2837a0bf547c4c
aristheg201 <arisgrindel@gmail.com>
2026-08-21 20:51:12 +0700

    feat: enable Flan claim bridge

--- lively-npcs-integration/src/main/java/vn/svframe/lively/integration/ServerEcosystemBootstrap.java
+++ lively-npcs-integration/src/main/java/vn/svframe/lively/integration/ServerEcosystemBootstrap.java
@@ -1,2 +1,17 @@
 package vn.svframe.lively.integration;
-import net.fabricmc.loader.api.FabricLoader;import vn.svframe.lively.api.LivelyApi;public final class ServerEcosystemBootstrap{private ServerEcosystemBootstrap(){}public static void install(){FabricLoader loader=FabricLoader.getInstance();if(loader.isModLoaded("luckperms"))LivelyApi.installPermissionBridge(LuckPermsBridge.create());if(loader.isModLoaded("beconomy"))LivelyApi.installEconomyBridge(BEconomyBridge.create());if(loader.isModLoaded("holodisplays"))LivelyApi.installHologramBridge(HoloDisplaysBridge.create());}}
+
+import net.fabricmc.loader.api.FabricLoader;
+import vn.svframe.lively.api.LivelyApi;
+
+/** Installs optional server ecosystem adapters without leaking them into Lively Core. */
+public final class ServerEcosystemBootstrap {
+    private ServerEcosystemBootstrap() {}
+
+    public static void install() {
+        FabricLoader loader = FabricLoader.getInstance();
+        if (loader.isModLoaded("luckperms")) LivelyApi.installPermissionBridge(LuckPermsBridge.create());
+        if (loader.isModLoaded("beconomy")) LivelyApi.installEconomyBridge(BEconomyBridge.create());
+        if (loader.isModLoaded("holodisplays")) LivelyApi.installHologramBridge(HoloDisplaysBridge.create());
+        if (loader.isModLoaded("flan")) LivelyApi.installClaimBridge(FlanClaimBridge.create());
+    }
+}
