888cc54efc74e681040f6a809c4dce9e752309f5
aristheg201 <arisgrindel@gmail.com>
2026-08-22 07:18:11 +0700

    test: expose SVF navigation manager double

---
+++ lively-npcs-integration/src/test/java/vn/svframe/waypoints/SvfWaypoints.java
@@ -0,0 +1,8 @@
+package vn.svframe.waypoints;
+
+import vn.svframe.waypoints.navigation.NavigationManager;
+
+public final class SvfWaypoints {
+    public static final NavigationManager NAVIGATION = new NavigationManager();
+    private SvfWaypoints() {}
+}
