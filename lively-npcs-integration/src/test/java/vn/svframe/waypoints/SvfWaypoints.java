92a9d4adacf4770d8a030ef4f2fc1adf00bae28c
aristheg201 <arisgrindel@gmail.com>
2026-08-22 08:00:40 +0700

    test(lively): mirror SVF Waypoints 1.2.17 root API

--- lively-npcs-integration/src/test/java/vn/svframe/waypoints/SvfWaypoints.java
+++ lively-npcs-integration/src/test/java/vn/svframe/waypoints/SvfWaypoints.java
@@ -3,6 +3,11 @@ package vn.svframe.waypoints;
 import vn.svframe.waypoints.navigation.NavigationManager;
 
 public final class SvfWaypoints {
-    public static final NavigationManager NAVIGATION = new NavigationManager();
+    private static final SvfWaypoints INSTANCE = new SvfWaypoints();
+    private final NavigationManager navigation = new NavigationManager();
+
     private SvfWaypoints() {}
+
+    public static SvfWaypoints instance() { return INSTANCE; }
+    public NavigationManager navigation() { return navigation; }
 }
