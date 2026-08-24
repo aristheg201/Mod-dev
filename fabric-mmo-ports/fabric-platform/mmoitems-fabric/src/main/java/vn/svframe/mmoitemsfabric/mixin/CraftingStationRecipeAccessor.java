5f8750f6a67a37adc7582d2fac2173f686fb40fc
aristheg201 <arisgrindel@gmail.com>
2026-08-24 20:07:53 +0700

    MMOItems: expose crafting recipe conditions

--- fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/mixin/CraftingStationRecipeAccessor.java
+++ fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/mixin/CraftingStationRecipeAccessor.java
@@ -3,8 +3,11 @@ package vn.svframe.mmoitemsfabric.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import java.util.List;
+
 @Mixin(targets = "vn.svframe.mmoitemsfabric.MMOItemsCraftingStationMod$Recipe", remap = false)
 public interface CraftingStationRecipeAccessor {
+    @Accessor("conditions") List<Object> mmoitems$getConditions();
     @Accessor("craftingTimeSeconds") int mmoitems$getCraftingTimeSeconds();
     @Accessor("outputItem") boolean mmoitems$getOutputItem();
     @Accessor("silentCraft") boolean mmoitems$getSilentCraft();
