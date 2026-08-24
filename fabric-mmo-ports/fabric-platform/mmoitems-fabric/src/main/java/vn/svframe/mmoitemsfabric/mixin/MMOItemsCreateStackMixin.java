250c900d5630408d30543c903529538f25fe75e1
aristheg201 <arisgrindel@gmail.com>
2026-08-24 17:24:14 +0700

    MMOItems: stamp current revision on newly created items

--- fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/mixin/MMOItemsCreateStackMixin.java
+++ fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/mixin/MMOItemsCreateStackMixin.java
@@ -7,12 +7,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vn.svframe.mmoitemsfabric.MMOItemsFabricMod;
 import vn.svframe.mmoitemsfabric.MMOItemsGameplayMod;
+import vn.svframe.mmoitemsfabric.MMOItemsRevisionRuntime;
 
 @Mixin(value = MMOItemsFabricMod.class, remap = false)
 public abstract class MMOItemsCreateStackMixin {
     @Inject(method = "createStack", at = @At("RETURN"), remap = false)
     private static void mmoitems$hydrateCreatedStack(String type, String id, int amount,
                                                      CallbackInfoReturnable<ItemStack> cir) {
-        MMOItemsGameplayMod.hydrate(cir.getReturnValue());
+        ItemStack stack = cir.getReturnValue();
+        MMOItemsGameplayMod.hydrate(stack);
+        MMOItemsRevisionRuntime.stampFresh(stack);
     }
 }
