05fefd79188c3f75b15361529f41aef3b91e4d31
aristheg201 <arisgrindel@gmail.com>
2026-08-24 17:27:03 +0700

    MMOCore: expose social managers to native Fabric bridges

--- fabric-mmo-ports/fabric-platform/mmocore-fabric/src/main/java/vn/svframe/mmocorefabric/mixin/MMOCoreFabricAccessor.java
+++ fabric-mmo-ports/fabric-platform/mmocore-fabric/src/main/java/vn/svframe/mmocorefabric/mixin/MMOCoreFabricAccessor.java
@@ -5,6 +5,8 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import vn.svframe.mmocorefabric.MMOCoreFabricMod;
 import vn.svframe.mmocorefabric.runtime.gameplay.ClassRuntime;
 import vn.svframe.mmocorefabric.runtime.progression.PlayerProgress;
+import vn.svframe.mmocorefabric.runtime.social.GuildManager;
+import vn.svframe.mmocorefabric.runtime.social.PartyManager;
 
 import java.util.Map;
 import java.util.UUID;
@@ -12,12 +14,14 @@ import java.util.UUID;
 @Mixin(value = MMOCoreFabricMod.class, remap = false)
 public interface MMOCoreFabricAccessor {
     @Accessor("PROFILES")
-    static Map<UUID, PlayerProgress> mmocore$getProfiles() {
-        throw new AssertionError();
-    }
+    static Map<UUID, PlayerProgress> mmocore$getProfiles() { throw new AssertionError(); }
 
     @Accessor("CLASSES")
-    static ClassRuntime mmocore$getClasses() {
-        throw new AssertionError();
-    }
+    static ClassRuntime mmocore$getClasses() { throw new AssertionError(); }
+
+    @Accessor("PARTIES")
+    static PartyManager mmocore$getParties() { throw new AssertionError(); }
+
+    @Accessor("GUILDS")
+    static GuildManager mmocore$getGuilds() { throw new AssertionError(); }
 }
