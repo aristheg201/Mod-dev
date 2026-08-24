d28dde7d394e980a428e376dcc215191f91f16d2
aristheg201 <arisgrindel@gmail.com>
2026-08-24 17:07:09 +0700

    MMOItems: fix requirement runtime collection parsing

--- fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/MMOItemsRequirementMod.java
+++ fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/MMOItemsRequirementMod.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
