dedf8df0a8bbc0bfef1e99b659708164c93631d1
aristheg201 <arisgrindel@gmail.com>
2026-08-24 16:49:44 +0700

    MMOItems: hydrate gameplay state on item creation

---
+++ fabric-mmo-ports/fabric-platform/mmoitems-fabric/src/main/java/vn/svframe/mmoitemsfabric/mixin/MMOItemsCreateStackMixin.java
@@ -0,0 +1,18 @@
+package vn.svframe.mmoitemsfabric.mixin;
+
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vn.svframe.mmoitemsfabric.MMOItemsFabricMod;
+import vn.svframe.mmoitemsfabric.MMOItemsGameplayMod;
+
+@Mixin(value = MMOItemsFabricMod.class, remap = false)
+public abstract class MMOItemsCreateStackMixin {
+    @Inject(method = "createStack", at = @At("RETURN"), remap = false)
+    private static void mmoitems$hydrateCreatedStack(String type, String id, int amount,
+                                                     CallbackInfoReturnable<ItemStack> cir) {
+        MMOItemsGameplayMod.hydrate(cir.getReturnValue());
+    }
+}
