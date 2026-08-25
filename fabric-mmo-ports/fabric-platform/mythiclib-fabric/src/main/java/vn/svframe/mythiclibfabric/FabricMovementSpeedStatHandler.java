42df1a019297fd7d907252084f4dcb82c695ccc9
aristheg201 <arisgrindel@gmail.com>
2026-08-25 07:44:42 +0700

    MythicLib: apply movement speed stats.yml settings

--- fabric-mmo-ports/fabric-platform/mythiclib-fabric/src/main/java/vn/svframe/mythiclibfabric/FabricMovementSpeedStatHandler.java
+++ fabric-mmo-ports/fabric-platform/mythiclib-fabric/src/main/java/vn/svframe/mythiclibfabric/FabricMovementSpeedStatHandler.java
@@ -5,8 +5,8 @@ import vn.svframe.mythiclibfabric.runtime.NativeStatEngine;
 
 /** Native Fabric counterpart of MythicLib 1.7.1 MovementSpeedStatHandler. */
 public final class FabricMovementSpeedStatHandler extends FabricAttributeStatHandler {
-    public FabricMovementSpeedStatHandler(NativeStatEngine engine) {
-        super("MOVEMENT_SPEED", EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1d);
+    public FabricMovementSpeedStatHandler(NativeStatEngine engine, MythicLibStatSettings.Entry settings) {
+        super("MOVEMENT_SPEED", EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1d, settings);
         setModifierEditor((instance, modifier) -> {
             if (modifier.value() >= 0.0d) return modifier;
             double reduction = engine.stat(instance.entityId(), "SPEED_MALUS_REDUCTION");
