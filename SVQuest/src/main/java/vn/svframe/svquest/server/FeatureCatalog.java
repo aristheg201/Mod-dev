4878d51408e602c4fc25d8c7475f9e54fd1cbad4
aristheg201 <arisgrindel@gmail.com>
2026-08-31 22:42:30 +0700

    SVQuest beta.6: add server-side feature whitelist

---
+++ SVQuest/src/main/java/vn/svframe/svquest/server/FeatureCatalog.java
@@ -0,0 +1,34 @@
+package vn.svframe.svquest.server;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class FeatureCatalog {
+    private FeatureCatalog() {}
+
+    public static final Map<String, String> COMMANDS = new LinkedHashMap<>();
+    static {
+        COMMANDS.put("pokemon_skills", "pokeskill");
+        COMMANDS.put("gts", "gts");
+        COMMANDS.put("skins", "trangphuc");
+        COMMANDS.put("shop", "shop");
+        COMMANDS.put("hunts", "hunts");
+        COMMANDS.put("raids", "raid list");
+        COMMANDS.put("ranked", "ranked");
+        COMMANDS.put("battle_tower", "battletower");
+        COMMANDS.put("battle_factory", "battlefactory");
+        COMMANDS.put("breeding", "breeding");
+        COMMANDS.put("wonder_trade", "wondertrade");
+        COMMANDS.put("sts", "sts");
+        COMMANDS.put("research", "research");
+        COMMANDS.put("expeditions", "expeditions");
+        COMMANDS.put("showcase", "showcase");
+        COMMANDS.put("daily", "daily");
+        COMMANDS.put("battle_pass", "battlepass");
+        COMMANDS.put("fusion", "fusion");
+        COMMANDS.put("homes", "home");
+        COMMANDS.put("warps", "warp");
+        COMMANDS.put("waypoints", "waypoint");
+        COMMANDS.put("claims", "claim");
+    }
+}
