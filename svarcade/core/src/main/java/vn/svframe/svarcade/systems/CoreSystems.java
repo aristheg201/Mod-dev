0de7db91f92f621858ba87fc5be244d21de38ef9
aristheg201 <arisgrindel@gmail.com>
2026-09-14 15:51:45 +0700

    feat(svarcade): register renderer and interaction systems

--- svarcade/core/src/main/java/vn/svframe/svarcade/systems/CoreSystems.java
+++ svarcade/core/src/main/java/vn/svframe/svarcade/systems/CoreSystems.java
@@ -10,10 +10,12 @@ import vn.svframe.svarcade.systems.combat.CombatSystem;
 import vn.svframe.svarcade.systems.currency.CurrencySystem;
 import vn.svframe.svarcade.systems.deployable.DeployableSystem;
 import vn.svframe.svarcade.systems.enemy.EnemySystem;
+import vn.svframe.svarcade.systems.interaction.InteractionSystem;
 import vn.svframe.svarcade.systems.loadout.LoadoutSystem;
 import vn.svframe.svarcade.systems.objective.ObjectiveSystem;
 import vn.svframe.svarcade.systems.path.PathSystem;
 import vn.svframe.svarcade.systems.presence.PresenceSystem;
+import vn.svframe.svarcade.systems.render.RendererSystem;
 import vn.svframe.svarcade.systems.shop.ShopSystem;
 import vn.svframe.svarcade.systems.spectator.SpectatorSystem;
 import vn.svframe.svarcade.systems.targeting.TargetingSystem;
@@ -65,6 +67,8 @@ public final class CoreSystems {
                 .add(WaveLoopSystem.ID, new WaveLoopSystem.Plan())
                 .add(SpectatorSystem.ID, new SpectatorSystem.Plan())
                 .add(PresenceSystem.ID, new PresenceSystem.Plan())
+                .add(RendererSystem.ID, new RendererSystem.Plan())
+                .add(InteractionSystem.ID, new InteractionSystem.Plan())
                 .add(BoardSystem.ID, new BoardSystem.Plan())
                 .add(BoardAdjudicationSystem.ID, new BoardAdjudicationSystem.Plan())
                 .add(ActionSystem.ID, new ActionSystem.Plan(actionHandlers))
